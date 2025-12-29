400f0c96d18d4f45584cfd87e1c4121c7f4eba21
AdejohOS <elusivebrown@gmail.com>
2025-12-29 12:43:45 +0100

    Initial commit

--- src/commands/deposit.js
+++ src/commands/deposit.js
@@ -3,13 +3,13 @@ import { Markup } from "telegraf";
 export async function depositCommand(ctx) {
   await ctx.answerCbQuery();
 
-  await ctx.reply(
-    "💰 *Choose Deposit Method*",
-    Markup.inlineKeyboard([
+  await ctx.editMessageText("💰 *Choose Deposit Method*", {
+    parse_mode: "Markdown",
+    ...Markup.inlineKeyboard([
       [Markup.button.callback("₿ Bitcoin (BTC)", "deposit_btc")],
       [Markup.button.callback("💵 USDT (TRC20)", "deposit_usdt_trc20")],
       [Markup.button.callback("💵 USDT (ERC20)", "deposit_usdt_erc20")],
       [Markup.button.callback("⬅ Back to Menu", "back_to_menu")],
-    ])
-  );
+    ]),
+  });
 }

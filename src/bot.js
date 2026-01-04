0fc21f3f967de0719deb696da1e2369353504096
AdejohOS <elusivebrown@gmail.com>
2026-01-04 17:16:34 +0100

    initial commit

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 import { Telegraf } from "telegraf";
 import dotenv from "dotenv";
-import session from "telegraf";
+import { session } from "telegraf/session";
 
 import { startCommand } from "./commands/start.js";
 import { depositMenu } from "./commands/depositMenu.js";

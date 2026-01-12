63c54b555bcb1c2b02efc1e3cc93240ea3f507b4
AdejohOS <elusivebrown@gmail.com>
2026-01-12 19:55:55 +0100

    initial commit

--- src/commands/escrow.js
+++ src/commands/escrow.js
@@ -9,11 +9,13 @@ export async function escrowMenu(ctx) {
     parse_mode: "Markdown",
     ...Markup.inlineKeyboard([
       [Markup.button.callback("➕ Make a Deal", "deal_create")],
-      [Markup.button.callback("⏳ Awaiting", "deal_pending")],
+
       [Markup.button.callback("📦 Active", "deal_active")],
       [Markup.button.callback("⚖ Disputes", "deal_disputes")],
-      [Markup.button.callback("✅ Completed", "deal_completed")],
-      [Markup.button.callback("⬅ Back to Main Menu", "main_menu")],
+      [
+        Markup.button.callback("⏳ Awaiting", "deal_pending"),
+        Markup.button.callback("✅ Completed", "deal_completed"),
+      ][Markup.button.callback("⬅ Back to Main Menu", "main_menu")],
     ]),
   });
 }

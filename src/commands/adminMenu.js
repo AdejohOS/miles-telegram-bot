8b53a80be4ac8d4146e8980baea3c3dc0b238b0d
AdejohOS <elusivebrown@gmail.com>
2026-01-12 11:20:38 +0100

    initial commit

--- src/commands/adminMenu.js
+++ src/commands/adminMenu.js
@@ -8,8 +8,12 @@ export async function adminMenu(ctx) {
   await ctx.editMessageText(text, {
     parse_mode: "Markdown",
     ...Markup.inlineKeyboard([
-      [Markup.button.callback("➕ Credit User", "admin_credit_menu")],
       [Markup.button.callback("🔍 Find User", "admin_find_user")],
+
+      [
+        Markup.button.callback("➕ Credit User", "admin_credit_menu"),
+        Markup.button.callback("➖ Debit User", "admin_debit_menu"),
+      ],
       [Markup.button.callback("💸 Withdrawals", "admin_withdrawals")],
       [Markup.button.callback("🛒 Shop Manager", "admin_shop_menu")],
 
